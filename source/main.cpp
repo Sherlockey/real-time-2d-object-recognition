573625cd15219de1a05c27f74f66a06a2c1b0fd0
Jonathon Davis <jondavis409@gmail.com>
2026-06-18 16:32:00 -0400

    build system set up

---
+++ source/main.cpp
@@ -0,0 +1,22 @@
+/*
+    Jonathon Davis
+    2026-06-18
+    Identifies a specified set of objects and shows the position and category of the object in an
+   output image. If provided a video sequence, it performs this in real time.
+*/
+
+#include <opencv2/opencv.hpp>
+
+/*
+    Program entry point
+
+    @param argc argument count with how many command line arguments provided
+    @param argv argument vector is an array of strings with command line args
+    @return exit status / return code sent back to OS
+*/
+int main(int argc, char* argv[])
+{
+    cv::VideoCapture* capdev;
+
+    return 0;
+}
